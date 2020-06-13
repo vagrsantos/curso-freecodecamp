3233d677d608910cb8f5a9639db83de0623f9012
Vagner Rodrigues <vagnerrodrigues0008@gmail.com>
2020-06-13 10:19:49 -0300

    Escaping Literal Quotes in Strings

--- my_script.js
+++ my_script.js
@@ -15,3 +15,27 @@ c = c + " String";
 
 var myVar = 87;
 myVar++;
+
+var myVar2 = 11;
+myVar2--;
+
+var myDecimal = 5.7; 
+var product2 = 2.0 * 2.5;
+var quotient2 = 4.4 / 2.0;
+var remainder = 11 % 3;
+
+var e = 3;
+e +=12;
+
+var f = 12;
+f -= 6;
+
+var ab = 2;
+ab *= 5;
+
+f /= 2;
+
+var myFirstname = "Vagner";
+var myLastName = "Santos";
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
