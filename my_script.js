12d0ccc565606db8208c74b66718a3c94f76c44b
Vagner Rodrigues <vagnerrodrigues0008@gmail.com>
2020-06-13 09:01:48 -0300

    Increment a Number with JavaScript

--- my_script.js
+++ my_script.js
@@ -1,7 +1,17 @@
 var a = 5;
 var b = 10;
 var c = "I am a";
+var studlyCapVar;
+
+var sum = 10 + 10;
+var difference = 45 - 33;
+var product = 8 * 10;
+var quotient = 66 / 33;
+studlyCapVar = 10;
 
 a = a + 1;
 b = b + 5;
 c = c + " String";
+
+var myVar = 87;
+myVar++;
